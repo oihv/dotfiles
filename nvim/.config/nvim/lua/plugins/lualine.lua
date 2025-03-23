f75e4d9bf914f1fd55435df50643137759ca9cdc
oihv <kokolas16@gmail.com>
2025-03-23 09:27:22 +0800

    Change section and component separators

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -6,9 +6,9 @@ return {
     require("lualine").setup({
       options = {
         icons_enabled = true,
-        theme = "pywal",
-        section_separators = { left = "", right = "" },
-        component_separators = { "", "" },
+        theme = "iceberg_dark",
+        section_separators = { left = " ", right = "" },
+        component_separators = { left = " ", right = "" },
         disabled_filetypes = {
           statusline = {},
           winbar = {},
