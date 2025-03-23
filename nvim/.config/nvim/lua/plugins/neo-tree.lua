243efa79dab5495d0e6936fff28cc81e0c0087ed
oihv <kokolas16@gmail.com>
2025-03-23 09:28:58 +0800

    Add mapping for open/close node, not working as of now

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,10 +1,20 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  -- require("neo-tree").setup({
+  --   filesystem = {
+  --     window = {
+  --       mappings = {
+  --         ["l"] = { "open_node" },
+  --         ["h"] = { "navigate_up" },
+  --       },
+  --     },
+  --   },
+  -- }),
 }
