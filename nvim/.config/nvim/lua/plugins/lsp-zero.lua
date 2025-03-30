05da56aaffc00bf1d1cf9f707c1d5267007296ce
oihv <kokolas16@gmail.com>
2025-03-30 20:01:23 +0800

    Add diagnostic config, and set it to virtual lines
    As of Neovim 0.11, the diagnostic is not by default and must be manually
    set, so this changes reflects that

--- nvim/.config/nvim/lua/plugins/lsp-zero.lua
+++ nvim/.config/nvim/lua/plugins/lsp-zero.lua
@@ -117,6 +117,16 @@ return {
           end,
         },
       })
+      vim.diagnostic.config({
+        -- Use the default configuration
+        virtual_lines = true,
+
+        -- Alternatively, customize specific options
+        -- virtual_lines = {
+        --  -- Only show virtual line diagnostics for the current cursor line
+        --  current_line = true,
+        -- },
+      })
     end,
   },
 }
